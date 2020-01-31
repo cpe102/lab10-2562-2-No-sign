6fc2e1e96747de3bd584cc6ebbfa6356e557cc4c
No-sign <55203083+No-sign@users.noreply.github.com>
2020-01-31 16:25:51 +0700

    Add files via upload

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+string grade[]={"A","B+","B","C+","C","D+","D","F","W"};
+
+int main(){
+	srand(time(0));
+	int x=rand();
+	cout<<"Press Enter 3 times to revel your future.";
+	cin.get();
+	cin.get();
+	cin.get();
+	cout<<"You will get "<<grade[x%9]<<" in this 261102";
+	return 0;
+	
+}
